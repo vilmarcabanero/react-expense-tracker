ccf465d6cb2f4df87c9cbaa6d0e69c20065f61ef
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-23 18:04:58 +0800

    32. Building a first custom component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+			<ExpenseItem></ExpenseItem>
+		</div>
+	)
 }
 
-export default App;
+export default App
