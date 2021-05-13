c460a7316bcd01782ea0bc0def2767f9f794ac1c
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-13 16:51:08 +0800

    47. How Component Functions Are Executed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = props => {
+
+	// let title = props.title
+	const [title, setTitle] = useState(props.title)
+
 	const clickHandler = () => {
-		console.log('Clicked!!!')
+		setTitle('Updated!')
+		console.log(title)
 	}
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.amount} </div>
 			</div>
 
